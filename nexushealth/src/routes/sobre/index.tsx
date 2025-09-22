c633144f9183d7ae811db73f4b6b2efd642a06e2
Erik <erikmiyasato@gmail.com>
2025-09-22 14:50:57 -0300

    Adicionado o icone do site, termiando a pagina sobre com o restante das informacoes

--- nexushealth/src/routes/sobre/index.tsx
+++ nexushealth/src/routes/sobre/index.tsx
@@ -1,4 +1,5 @@
 import Fachada from "../../img/fachada-hc.jpg";
+import Medico from "../../img/medicos-hc.jpg"
 
 export default function Sobre() {
     document.title = "Sobre - Nexus Health";
@@ -21,6 +22,20 @@ export default function Sobre() {
                         desenvolve estudos em diversas especialidades das ciências da saúde, por meio de seus 62
                         Laboratórios de Investigação Médica.
                     </p>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <img src={Medico} alt="Foto de Médicos do Hospital das Clínicas" />
+                    <p>Desde que foi inaugurado em 19 de abril de 1944, o Complexo Hospital das Clínicas da Faculdade de
+                        Medicina da USP se destaca como um dos principais centros do Brasil na geração e divulgação de
+                        conhecimento técnico e científico. Reconhecido por sua excelência, é uma referência nacional nas
+                        áreas de ensino, pesquisa e atendimento à saúde. Com uma infraestrutura de cerca de 380 mil
+                        metros quadrados, o complexo dispõe de aproximadamente dois mil leitos e conta com o trabalho de
+                        15 mil profissionais de diversas áreas. Sua composição inclui sete institutos, dois hospitais de
+                        apoio, diversos laboratórios de pesquisa médica, unidades especializadas e espaços de suporte,
+                        como o edifício administrativo, o Centro de Convenções Rebouças e a Escola de Educação
+                        Permanente.</p>
                 </div>
             </section>
         </main>
