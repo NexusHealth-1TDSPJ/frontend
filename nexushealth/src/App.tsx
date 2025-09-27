af3b604e4128013143f5e2f5d945617b00c95a90
JoaoHBatistaLeal <jhbatistaleal@gmail.com>
2025-09-27 13:30:23 -0300

    fix(globals.css): corrige barra branca sem conteudo relacionada a classe .container

--- nexushealth/src/App.tsx
+++ nexushealth/src/App.tsx
@@ -4,7 +4,7 @@ import Rodape from "./components/Rodape/Rodape";
 
 export default function App() {
   return (
-    <div className="container">
+    <div>
       <Cabecalho />
       <Outlet />
       <Rodape />
