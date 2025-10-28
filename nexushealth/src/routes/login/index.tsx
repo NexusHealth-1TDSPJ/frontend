4b85923708002ae87b24be4c279606f9482cde60
Hector <hectorsoria2@outlook.com>
2025-10-28 11:01:21 -0300

    feat(login):Criacao inicial da pagina de login

--- nexushealth/src/routes/login/index.tsx
+++ nexushealth/src/routes/login/index.tsx
@@ -1,3 +1,35 @@
-export default function LoginPage() {
-  return <div>Login Page</div>;
+import { Link } from "react-router-dom";
+export default function Login() {
+  document.title = "Login";
+
+  return (
+    <main >
+      <div >
+        <h1>
+          Login
+        </h1>
+
+        <form >
+          <div>
+            <label >
+              E-mail
+            </label>
+          </div>
+
+          <div>
+            <label >
+              Senha
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 }
