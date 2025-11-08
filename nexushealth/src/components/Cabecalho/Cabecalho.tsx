922774b27a06c979982675682bd5188b29b8929e
erikmiyasato <erikmiyasato@gmail.com>
2025-11-07 22:13:21 -0300

    feat(cabecalho)!: remove nav caso o usuario nao esteja autenticado

--- nexushealth/src/components/Cabecalho/Cabecalho.tsx
+++ nexushealth/src/components/Cabecalho/Cabecalho.tsx
@@ -6,47 +6,52 @@ import { useEffect, useState } from "react";
 export default function Cabecalho() {
   const navigate = useNavigate();
   const [primeiroNome, setPrimeiroNome] = useState<string>("");
+  const [autenticado, setAutenticado] = useState<boolean>(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) return;
 
+    setAutenticado(true);
+
     const fetchUser = async () => {
       try {
-        const response = await fetch("colocar o endpoint depois que fizerem o deploy do backend (GET com nome do paciente)", {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await fetch(
+          "colocar o endpoint depois que fizerem o deploy do backend (GET com nome do paciente)",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,},});
 
         if (!response.ok) {
-          throw new Error("Erro ao buscar paciente");
-        }
+          throw new Error("Erro ao buscar paciente");}
 
         const data = await response.json();
         const primeiro = data.nome?.split(" ")[0] || "";
         setPrimeiroNome(primeiro);
       } catch (error) {
-        console.error(error);
-      }};
+        console.error(error);}};
 
-    fetchUser();}, []);
+    fetchUser();},[]);
 
   return (
     <header className="w-full bg-[#a2ffe9] text-slate-900 flex flex-wrap justify-between items-center py-4 px-4 sm:px-6 md:px-8 lg:px-16 z-20 relative shadow-sm">
       <h1 className="text-2xl font-bold tracking-tight">Nexus Health</h1>
-      <div className="flex items-center gap-4">
-        <Menu />
-        {primeiroNome && (
-          <div className="flex items-center gap-2">
-            <span className="font-medium">{primeiroNome}</span>
-            <button
-              onClick={() => navigate("/editar-usuario")}
-              className="text-slate-900 hover:text-slate-700"><HiOutlineUserCircle size={28} />
-            </button>
-          </div>)}
-      </div>
+
+      {autenticado && (
+        <div className="flex items-center gap-4">
+          <Menu />
+          {primeiroNome && (
+            <div className="flex items-center gap-2">
+              <span className="font-medium">{primeiroNome}</span>
+              <button
+                onClick={() => navigate("/editar-usuario")}
+                className="text-slate-900 hover:text-slate-700">
+                <HiOutlineUserCircle size={28}/>
+              </button>
+            </div>)}
+        </div>
+    )}
     </header>
   );
 }
